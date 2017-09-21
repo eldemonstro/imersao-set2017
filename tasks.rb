e1d3be97f5200baee2cb01b4c7f26ccdb92db8bd
Fabricio Bezerra <demonstro003@gmail.com>
2017-09-21 13:18:20 -0300

    Agora tem objeto tarefa :fire:

--- tasks.rb
+++ tasks.rb
@@ -1,27 +1,50 @@
+require './task'
+
+INSERIR_TAREFA = 1
+MOSTRAR_TAREFA = 2
+BUSCAR_TAREFA = 3
+FINALIZAR_TAREFA = 4
+SAIR = 5
+
+def menu
+  puts "\nEscolha uma opção:"
+  puts "##{INSERIR_TAREFA} - Inserir uma tarefa"
+  puts "##{MOSTRAR_TAREFA} - Mostrar todas as tarefas"
+  puts "##{BUSCAR_TAREFA} - Busca uma tarefa"
+  puts "##{FINALIZAR_TAREFA} - Marcar tarefa como finalizada"
+  puts "##{SAIR} - Sair"
+  gets.to_i
+end
+
+def print_tarefas(tarefas)
+  if tarefas.empty?
+    puts "N\xC3\xA3o h\xC3\xA1 tarefas"
+  elsif
+    tarefas.each(&:print_tarefa)
+  end
+end
+
 puts 'Bem-vindo ao Task List'
 opcao = 0
 tarefas = []
 
-while opcao != 4 do
-  if opcao == 1
+while opcao != SAIR
+  if opcao == INSERIR_TAREFA
     puts "\nInforme o nome da tarefa"
     nome_tarefa = gets.chomp
-    tarefas << {nome: nome_tarefa, finalizada: false, id: tarefas.length}
+    tarefas << Task.new(nome_tarefa, tarefas.length)
     puts 'Nova tarefa cadastrada: ' + nome_tarefa
-  elsif opcao == 2
+  elsif opcao == MOSTRAR_TAREFA
     puts "\nSuas tarefas sao:"
-    tarefas.each {|tarefa| puts "A tarefa: #{tarefa[:nome]} de id #{tarefa[:id]} #{tarefa[:finalizada] ? "está finalizada" : "não está finalizada"}", puts}
-  elsif opcao == 3
+    print_tarefas tarefas
+  elsif opcao == BUSCAR_TAREFA
+    puts "\nBuscando tarefas\nDigite o nome da tarefa a buscar"
+    texto_busca = gets.chomp.downcase
+    tarefas_encontradas = tarefas.select { |tarefa| tarefa.nome.downcase.include? texto_busca.downcase }
+    print_tarefas(tarefas_encontradas)
+  elsif opcao == FINALIZAR_TAREFA
     puts "\nInforme o id da tarefa a marcar como finalizada"
-    tarefas[tarefas.index {|tarefa| tarefa[:id] == gets.to_i}][:finalizada] = true
+    tarefas[tarefas.index { |tarefa| tarefa.id == gets.to_i }].done = true
   end
-
-  puts
-  puts 'Escolha uma opção:'
-  puts '#1 - Inserir uma tarefa'
-  puts '#2 - Mostrar todas as tarefas'
-  puts '#3 - Marcar tarefa como finalizada'
-  puts '#4 - Sair'
-
-  opcao = gets.to_i
+  opcao = menu
 end
